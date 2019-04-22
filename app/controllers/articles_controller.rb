2be0110443c2ecd8c395db8921ed7bd6b4d87c04
Sean <me@ininger.me>
2019-04-22 11:40:34 -0700

    troubleshooting

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,27 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
   def index
     @articles = Article.all #for controller and view to both see articles it must be an instance variable
   end
   def show
+    @article = Article.find(params[:id])
+  end
+  def new
+    @article = Article.new
+  end
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+  end
+  def destroy
+      redirect_to article_path(@article)
+    @article = Article.find(params[:id])
+
+    @article.destroy
+
+  end
+  def edit
     @article = Article.find(params[:id])
   end
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,6 @@
 module ArticlesHelper
+  #strong parameters per http://tutorials.jumpstartlab.com/projects/blogger.html
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
