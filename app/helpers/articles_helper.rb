dccef5f0dd5453786dc3d7eee56485c971a627c9
Sean <me@ininger.me>
2019-04-23 19:20:04 -0700

    error appears related to incorrect pathing

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,6 +1,6 @@
 module ArticlesHelper
   #strong parameters per http://tutorials.jumpstartlab.com/projects/blogger.html
   def article_params
-    params.require(:article).permit(:title, :body, :tag_list)
+    params.require(:article).permit(:title, :body, :tag_list, :image)
   end
 end
